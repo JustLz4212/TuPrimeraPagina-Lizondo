4697e200125ff351098bba5a185d089ade097aed
JustoLz <justolizondo@gmail.com>
2025-07-15 20:53:21 -0300

    soluciona errores

--- Entrega3/models.py
+++ Entrega3/models.py
@@ -2,16 +2,25 @@ from django.db import models
 
 class Piloto(models.Model):
     nombre = models.CharField(max_length=100)  # Campo string de 100 caracteres
-    # escuderia = models.CharField(max_length=100)
+    escuderia = models.CharField(max_length=100)
     Puntos = models.IntegerField()  # Campo entero
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Escudería: {self.escuderia} - Puntos: {self.Puntos}"
 
 class Escuderia(models.Model):
     nombre = models.CharField(max_length=30)  # Campo string de 30 caracteres
     TeamPrincipal = models.CharField(max_length=30)  # Campo string de 100 caracteres
     puesto = models.IntegerField()  # Campo entero
 
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Director: {self.TeamPrincipal} - Puesto: {self.puesto}"
+
 class Campeones(models.Model):
     nombre = models.CharField(max_length=30)  # Campo string de 30 caracteres
     Campeonatos = models.IntegerField()  
-    victorias = models.IntegerField()  
+    victorias = models.IntegerField() 
+
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Campeonatos: {self.Campeonatos} - Victorias: {self.victorias}"
     
